85828a82fb5ca42d2084045ddfc4e186e3acd9a0
Jan Christopher Francisco <152581275+jcfrancisco0103@users.noreply.github.com>
2024-02-08 16:04:32 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "myDB";
 
 // For socitcloud
 $servername = "localhost";
